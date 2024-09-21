a218104bf1acc89ef666b53fd04e6bbbc7ef6ad8
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-09-22 00:03:47 +0300

    add device_preview and flutter_screenutil package

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,42 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:marsa_tours/views/splash_view.dart';
 import 'package:marsa_tours/widgets/on_boarding_body.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp (DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MarsaTours(), // Wrap your app
+  ),
+  );
+
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MarsaTours extends StatelessWidget {
+  const MarsaTours({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+    minTextAdapt: true,
+    splitScreenMode: true,
+    // Use builder only if you need to use library outside ScreenUtilInit context
+    builder: (_ , child) {
+          return  MaterialApp(
       debugShowCheckedModeBanner: false,
+            useInheritedMediaQuery: true,
+            locale: DevicePreview.locale(context),
+            builder: DevicePreview.appBuilder,
+            theme: ThemeData.light(),
+            darkTheme: ThemeData.dark(),
       home: OnBoardingBody(),
     );
   }
+    );
+}
 }
 
